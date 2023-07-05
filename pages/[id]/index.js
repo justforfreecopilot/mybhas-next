d09ec2c26dcf2b45f3c9dd4231c2c14b3da4f467
ekavadedattaprasad <138019916+ekavadedattaprasad@users.noreply.github.com>
2023-07-05 16:46:02 +0530

    routes with data fetching from URL

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,3 +1,6 @@
-export default function Id() {
-  return <div>Id</div>;
+import { useRouter } from 'next/router'
+ 
+export default function PageSlug() {
+  const router = useRouter()
+  return <p>Not About: {router.query.id}</p>
 }
