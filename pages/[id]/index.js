7d0f35a269d6bfe93000762fb465ff30a7e557e0
ekavadedattaprasad <138019916+ekavadedattaprasad@users.noreply.github.com>
2023-07-05 17:16:56 +0530

    SSR Example

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,6 +1,17 @@
-import { useRouter } from 'next/router'
- 
-export default function PageSlug() {
-  const router = useRouter()
-  return <p>Not About: {router.query.id}</p>
+import { useRouter } from "next/router";
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://v2.jokeapi.dev/joke/Any");
+  const repo = await res.json();
+  return { props: { repo } };
+};
+
+export default function PageSlug({ repo }) {
+  const router = useRouter();
+  return (
+    <p>
+      Not About: {router.query.id} <br /> And some content:{" "}
+      {repo.joke ? repo.joke : repo.setup + ": " + repo.delivery}
+    </p>
+  );
 }
